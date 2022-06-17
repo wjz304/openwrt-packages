3819e1bc29c49f5bebe9575af122fb1493bedf8e
Ing <wjz304@qq.com>
2022-06-17 22:42:30 +0800

    init

--- upgrade.sh
+++ upgrade.sh
@@ -1,80 +1,35 @@
-
 #!/bin/bash
 
 function git_clone() {
-  git clone --depth 1 $1 $2 || true
- }
-function git_sparse_clone() {
-  branch="$1" rurl="$2" localdir="$3" && shift 3
-  git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
-  cd $localdir
-  git sparse-checkout init --cone
-  git sparse-checkout set $@
-  mv -n $@ ../
-  cd ..
-  rm -rf $localdir
-  }
-function mvdir() {
-mv -n `find $1/* -maxdepth 0 -type d` ./
-rm -rf $1
+  rm -rf $(basename $1 .git)
+  git clone --depth 1 $1 $(basename $1 .git) || true
+  rm -rf $(basename $1 .git)/.svn* $(basename $1 .git)/.git*
+}
+
+function svn_co() {
+  rm -rf $2
+  svn co $1 $2 || true
+  rm -rf $2/.svn* $2/.git*
 }
 
 
+git_clone https://github.com/jerrykuku/luci-theme-argon
 
-
-git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon
-git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config
+git_clone https://github.com/jerrykuku/luci-app-argon-config
 sed -i 's/\(+luci-compat\)/\1 +luci-theme-argon/' luci-app-argon-config/Makefile
 
-git clone --depth 1 https://github.com/jerrykuku/lua-maxminddb.git
-git clone --depth 1 https://github.com/jerrykuku/luci-app-vssr.git
 
-#git clone https://github.com/jerrykuku/go-aliyundrive-webdav.git
-#git clone https://github.com/jerrykuku/luci-app-go-aliyundrive-webdav.git  
-
-#git clone --depth=1 https://github.com/fw876/helloworld.git
+git_clone https://github.com/jerrykuku/lua-maxminddb.git
+git_clone https://github.com/jerrykuku/luci-app-vssr.git
 
 
-
-git clone --depth 1 https://github.com/ntlf9t/luci-app-easymesh
-git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot
-git clone --depth 1 https://github.com/shanglanxin/luci-app-homebridge
-git clone --depth 1 https://github.com/esirplayground/luci-app-poweroff
-git clone --depth 1 https://github.com/Jason6111/luci-app-netdata
-
-git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall passwall1 && mv -n passwall1/luci-app-passwall  ./; rm -rf passwall1
-
-svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-filebrowser
-svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-aliddns
-svn export https://github.com/immortalwrt/packages/trunk/net/smartdns
-svn export https://github.com/Tencent-Cloud-Plugins/tencentcloud-openwrt-plugin-ddns/trunk/tencentcloud_ddns luci-app-tencentddns
-svn export https://github.com/Tencent-Cloud-Plugins/tencentcloud-openwrt-plugin-cos/trunk/tencentcloud_cos luci-app-tencentcloud-cos
-svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome
-svn export https://github.com/kiddin9/openwrt-packages/trunk/adguardhome
-
-svn export https://github.com/linkease/istore/trunk/luci/luci-app-store
-svn export https://github.com/linkease/istore-ui/trunk/app-store-ui
+git_clone https://github.com/linkease/istore-ui
+git_clone https://github.com/linkease/istore && mv -n istore/luci/* ./; rm -rf istore
 sed -i 's/luci-lib-ipkg/luci-base/g' luci-app-store/Makefile
 
-svn export https://github.com/linkease/nas-packages/trunk/network/services && mvdir services
-svn export https://github.com/linkease/nas-packages-luci/trunk/luci && mvdir luci
-
-svn export https://github.com/Lienol/openwrt-package/branches/other/lean/luci-app-autoreboot
-svn export https://github.com/fw876/helloworld/trunk/sagernet-core
-svn export https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus
-
-svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
-sed -i 's/+libcap /+libcap +libcap-bin /' luci-app-openclash/Makefile
-
-svn export https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
-sed -i 's/+dockerd/+dockerd +cgroupfs-mount/' luci-app-docker*/Makefile
-sed -i '$i /etc/init.d/dockerd restart &' luci-app-docker*/root/etc/uci-defaults/*
 
 
-rm -rf ./*/.svn
-rm -rf ./*/.git
-rm -rf ./*/.github 
-rm -rf ./*/.gitignore
-rm -f ./*/.gitattributes
+rm -rf ./*/.svn*
+rm -rf ./*/.git*
 
 exit 0
