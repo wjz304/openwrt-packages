a2cae594662849841811f462c99570aa784cc219
Ing <wjz304@qq.com>
2022-06-18 00:29:08 +0800

    init

--- upgrade.sh
+++ upgrade.sh
@@ -6,6 +6,12 @@ function git_clone() {
   rm -rf $(basename $1 .git)/.svn* $(basename $1 .git)/.git*
 }
 
+function git_clone_b() {
+  rm -rf $(basename $1 .git)
+  git clone --depth 1 -b $2 $1 $(basename $1 .git) || true
+  rm -rf $(basename $1 .git)/.svn* $(basename $1 .git)/.git*
+}
+
 function svn_co() {
   rm -rf $2
   svn co $1 $2 || true
@@ -13,11 +19,14 @@ function svn_co() {
 }
 
 
-git_clone https://github.com/jerrykuku/luci-theme-argon
+git_clone_b https://github.com/jerrykuku/luci-theme-argon 18.06
 
 git_clone https://github.com/jerrykuku/luci-app-argon-config
 sed -i 's/\(+luci-compat\)/\1 +luci-theme-argon/' luci-app-argon-config/Makefile
 
+
+git_clone https://github.com/fw876/helloworld
+git_clone https://github.com/xiaorouji/openwrt-passwall
 
 git_clone https://github.com/jerrykuku/lua-maxminddb.git
 git_clone https://github.com/jerrykuku/luci-app-vssr.git
