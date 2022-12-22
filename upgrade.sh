83123ff56f79f7b1c87f60d8f646f8e80f825633
Ing <wjz304@qq.com>
2022-12-22 16:53:59 +0800

    mod zh_cn to zh-cn

--- upgrade.sh
+++ upgrade.sh
@@ -158,13 +158,14 @@ git_clone https://github.com/NateLol/luci-app-beardropper
 # IP限速
 svn_co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos
 sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' luci-app-eqos/Makefile
-cp -rf luci-app-eqos/po/zh_Hans luci-app-eqos/po/zh_cn
+cp -rf luci-app-eqos/po/zh_Hans luci-app-eqos/po/zh-cn
+sed -i 's/zh_Hans/zh_CN/g' luci-app-eqos/po/zh-cn/eqos.po
 
 
 # 文件浏览器
 git_clone https://github.com/xiaozhuai/luci-app-filebrowser
 sed -i 's/"services"/"nas"/g; s/"Services"/"NAS"/g' luci-app-filebrowser/luasrc/controller/filebrowser.lua
-cp -rf luci-app-filebrowser/po/zh_Hans luci-app-filebrowser/po/zh_cn
+cp -rf luci-app-filebrowser/po/zh_Hans luci-app-filebrowser/po/zh-cn
 
 
 # gowebdav
@@ -174,7 +175,7 @@ svn_co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav
 sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' luci-app-gowebdav/Makefile
 #sed -i '/"NAS"/d; /page/d' luci-app-gowebdav/luasrc/controller/gowebdav.lua
 #sed -i 's/\"nas\"/\"services\"/g' luci-app-gowebdav/luasrc/controller/gowebdav.lua
-cp -rf luci-app-gowebdav/po/zh_Hans luci-app-gowebdav/po/zh_cn
+cp -rf luci-app-gowebdav/po/zh_Hans luci-app-gowebdav/po/zh-cn
 
 
 # vm-tools
